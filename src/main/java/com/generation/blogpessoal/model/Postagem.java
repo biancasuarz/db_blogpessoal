1e104f1a5c8a51dabcd38ffa083b2773a804ea65
USER <blancasuarz@gmail.com>
2025-01-14 18:19:17 -0300

    implementando model | usuario

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -41,6 +41,10 @@ public class Postagem {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -82,4 +86,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}	
+	
 }
